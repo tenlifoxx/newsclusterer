ce14f07476d1c0e6d88aec722d3db0697b56b8fd
yuma <tenlifoxx@gmail.com>
2021-09-02 11:33:01 +0900

    updated crawler

--- Crawler/main.py
+++ Crawler/main.py
@@ -1,31 +1,48 @@
-import requests
-from bs4 import BeautifulSoup
+import crawl
+import os
+import time
+import json
 
 
-def urlList(press): ### 크롤링 할 언론사 URL 리스트 생성
-    url_list = []
-    for p in press:
-        url_list.append()
+def writeFile(name, serial, content):
+    file_name = name + '_' + serial + '.txt'
+    file = open(os.path.join('data', file_name), 'w')
+    file.write(content[1])
+
+    file.close()
+
+    return file_name
 
 
-
-def makeUrl(officeId):  ### 네이버 뉴스 랭킹에서 언론사별 URL 생성
-    url = "https://news.naver.com/main/ranking/office.naver?"
-    params = {
-        "officeId": officeId
-    }
-    raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, params=params)
-
-    return raw.text
+def writeInfo(serial, info):
+    file_name = 'info' + '_' + serial + '.json'
+    file = open(os.path.join('data', file_name), 'w')
+    file.write(json.dumps(info, ensure_ascii=False))
+    file.close()
 
 
-def crawlFromUrl(url):
-    html = BeautifulSoup(url, "html.parser")
+if __name__ == "__main__":
+    start = time.time()
+    press_no = {'중앙': '025', 'JTBC': '437', 'YTN': '052', '연합': '001', 'SBS': '055',
+                'KBS': '056', 'MBC': '214', '한겨레': '028', '경향': '032', '조선': '023'}
+    files = {}
 
+    links = crawl.linksOfPress(press_no)
+    print("|" * 200)
 
-def newPageList(press):
-    pass
+    for key, value in links.items():
+        for e in value:
+            fileInfo = {}
+            now = str(time.time())
+            crawled = crawl.parseNews(crawl.makeHTML(e[1], {}))
+            file_name = writeFile(key, now, crawled)
 
+            fileInfo['title'] = e[0]
+            fileInfo['url'] = e[1]
+            files[file_name] = fileInfo
 
-press_no = {'중앙': '025', 'JTBC': '437', 'YTN': '052', '연합': '001', 'SBS': '055',
-            'KBS': '056', 'MBC': '214', '한겨레': '028', '경향': '032', '조선': '023'}
+            print("|", end='')
+
+    writeInfo(time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())), files)
+    print('\n\n경과 시간 : ', end='')
+    print(time.time()-start)
