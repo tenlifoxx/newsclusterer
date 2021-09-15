24467a105e51670deae4c9d1e07886ed31770618
junsung9889 <junsung9889@gmail.com>
2021-09-15 10:05:50 +0900

    update#2

---
+++ Web/lib/mongodb.js
@@ -0,0 +1,59 @@
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}
+
+let client
+let clientPromise
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local')
+}
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
+
+let cached = global.mongo
+
+if(!cached) {
+  cached = global.mongo = {conn: null, promise: null}
+}
+
+export async function connectToDatabase() {
+  if(cached.conn){
+    return cached.conn
+  }
+  if(!cached.promise){
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+
+    cached.promise = MongoClient.connect(process.env.MONGODB_URI, opts).then((client) =>{
+      return{
+        client,
+        db: client.db(process.env.MONGODB_DB),
+      }
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
+}
